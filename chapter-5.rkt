e9bbe044ec54f34d703bea86c7aa9986a2f15736
meowu <474384902@qq.com>
2020-02-25 17:59:04 +0800

    chapter 5.

--- chapter-5.rkt
+++ chapter-5.rkt
@@ -7,6 +7,28 @@
 (atom? 1); #t
 (atom? (quote())); #f
 
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define sub
+  (lambda (n m)
+    (cond 
+      ((zero? m) n)
+      (else
+        (sub1 (sub n (sub1 m)))))))
+
+(define add
+  (lambda (n m)
+    (cond 
+      ((zero? m) n)
+      (else 
+        (add1 (add n (sub1 m)))))))
+
 (define > 
   (lambda (n m)
     (cond
@@ -61,3 +83,99 @@
 (rember* 1 '(1 2 3))
 
 (rember* 1 (list (list 1 2 3) 4 5 6 1 (list 1 7 8)))
+
+
+(define insertR*
+ (lambda (new old lat)
+  (cond
+   ((null? lat) (quote()))
+    ((atom? (car lat))
+      (cond
+       ((eq? (car lat) old)
+        (cons (car lat) 
+               (cons new (insertR* new old (cdr lat)))))
+        (else 
+         (cons (car lat) (insertR* new old (cdr lat))))))
+      (else
+       (cons (insertR* new old (car lat)) 
+        (insertR* new old (cdr lat)))))))
+
+(define l (list 
+           (list "how" "much" 
+            (cons "wood" null)) 
+             "could"
+              (list (list "a" '("wood") "chuck"))
+              '('('("chuck")))
+               (list "if" (list "a") (list (list "wood" "chuck")))
+                "could" "chuck" "wood"))
+(insertR* "roast" "chuck" l)
+
+
+(define occur*
+  (lambda (a l)
+    (cond
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a)
+          (add1 (occur* a (cdr l))))
+         (else
+          (occur* a (cdr l)))))
+      (else
+       (add (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define ll '('("banana") '("split" '('('('("banana" "ice")))
+                                    '("cream" '("banana"))
+                                    "sherbet"))
+                        '("banana")
+                        '("bread")
+                        '("banana" "brandy")))
+
+(occur* "banana" ll)
+(occur* "Apple" '("Apple" "Egg" "Bear" "Apple" "Banana"))
+ 
+(define subst*
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote()))
+      ((atom? (car lat))
+       (cond
+         ((eq? (car lat) old)
+          (cons new (subst* new old (cdr lat))))
+       (else
+        (cons (car lat) (subst* new old (cdr lat))))))
+     (else
+      (cons (subst* new old (car lat)) (subst* new old (cdr lat)))))))
+
+
+(define occurl '('("banana") '("split" '('('('("banana" "ice")))
+                                         '("cream" '("banana"))
+                                         "sherbet"))
+                             '("banana")
+                             '("brean")
+                             '("banana" "brandy")))
+(subst* "orange" "banana" occurl)
+
+(define insertL*
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((atom? (car lat))
+       (cond
+         ((eq? old (car lat))
+          (cons new (cons old (insertL* new old (cdr lat)))))
+         (else
+          (cons old (insertL* new old (cdr lat))))))
+      (else
+       (cons (insertL* new old (car lat)) (insertL* new old (cdr lat)))))))
+
+(define insertl '('("banana") '("split" '('('('("banana" "ice")))
+                                         '("cream" '("banana"))
+                                         "sherbet"))
+                             '("banana")
+                             '("brean")
+                             '("banana" "brandy")))
+
+(insertL* "apple" "banana" insertl)
+
+
